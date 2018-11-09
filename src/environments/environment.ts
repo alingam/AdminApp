f9fcce36d7de771638e4ab2df5b23ab6e353576f
Lingam,Aparna <aparna.lingam@vizientinc.com>
2018-11-09 13:47:51 -0600

    Initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+export const environment = {
+    production: false,
+
+    region: 'us-east-1',
+
+    identityPoolId: 'us-east-1:7e5b9b7d-803a-4e2b-a0eb-871defd1c078',
+    userPoolId: 'us-east-1_46Fj1uUOo',
+    clientId: 'gq0dun2esrc7fmti42rc2m9be',
+
+    gateway: 'https://sx28mxsrhj.execute-api.us-east-1.amazonaws.com/SparkAdmin',
+
+    rekognitionBucket: 'rekognition-pics',
+    albumName: 'usercontent',
+    bucketRegion: 'us-east-1',
+
+    ddbTableName: 'LoginTrail',
+
+    cognito_idp_endpoint: '',
+    cognito_identity_endpoint: '',
+    sts_endpoint: '',
+    dynamodb_endpoint: '',
+    s3_endpoint: ''
+
+};
+
